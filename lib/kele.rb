115965cb775f0d69932fa7e9923787ba5ecb5b1e
Jessica Schultz <jessicaschultz1203@gmail.com>
2018-03-05 17:14:25 -0800

    completed 2&3. initialize, authorize, and retrieve user

---
+++ lib/kele.rb
@@ -0,0 +1,24 @@
+require 'httparty'
+require 'json'
+
+class Kele
+  include HTTParty
+
+  base_uri 'https://www.bloc.io/api/v1'
+
+  def initialize(email, password)
+    post_response = self.class.post('/sessions', body: {
+        email: email,
+        password: password
+      })
+    @user_auth_token = post_response['auth_token']
+    raise "Invalid Email or Password. Try Again." if @user_auth_token.nil?
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @user_auth_token })
+    JSON.parse(response.body)
+  end
+
+
+end
